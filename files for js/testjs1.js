3d1021a37935841c3c4c29b046e27052051f148a
rbtsoheib <taharsoheib08@gmail.com>
2024-06-30 22:55:01 +0200

    new commits

---
+++ files for js/testjs1.js
@@ -0,0 +1,40 @@
+let x= 5 
+console.log(x)
+
+let y= 6 
+console.log(y)
+
+
+console.log(typeof x)
+console.log(typeof y)
+
+
+console.log( y + x )
+console.log(x - y )
+console.log(x * y)
+console.log(x / y)
+console.log(x % y)
+
+if (x < 4){
+
+console.log("x is playing dangerously")
+
+}
+else {
+
+console.log("x is a good guy")
+
+}
+
+if (y > 6){
+
+console.log("y is a bad boy for that")
+
+}
+
+else {
+
+console.log("y is a f boy for that")
+
+}
+
